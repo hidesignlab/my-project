801cbdbeec9298e3f64a4b0c904217764cc0ff76
hidesignlab <hammadiqbal.1@gmail.com>
2024-10-14 08:14:34 +0300

    Update script.js

--- embryo/script.js
+++ embryo/script.js
@@ -7,12 +7,11 @@ document.addEventListener('DOMContentLoaded', () => {
         const x = event.gamma; // left to right, ranges from -90 to 90
         const y = event.beta;  // front to back, ranges from -180 to 180
 
-        // Adjust these values for the desired movement speed
         const moveFactor = 10;
 
         // Apply a parallax effect to each layer with different intensities
-        layer1.style.transform = `translate(${x / moveFactor}px, ${y / moveFactor}px)`;
-        layer2.style.transform = `translate(${x / (moveFactor * 2)}px, ${y / (moveFactor * 2)}px)`;
-        layer3.style.transform = `translate(${x / (moveFactor * 3)}px, ${y / (moveFactor * 3)}px)`;
+        layer1.style.transform = `scale(1) translate(${x / moveFactor}px, ${y / moveFactor}px)`;
+        layer2.style.transform = `scale(0.3) translate(${x / (moveFactor * 2)}px, ${y / (moveFactor * 2)}px)`;
+        layer3.style.transform = `scale(0.3) translate(${x / (moveFactor * 3)}px, ${y / (moveFactor * 3)}px)`;
     });
 });
